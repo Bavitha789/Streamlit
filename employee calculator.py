3a10e1f3843d85700fea22ee424daffe12db1400
Bavitha789 <144424274+Bavitha789@users.noreply.github.com>
2023-11-23 16:48:21 +0530

    employee calculator.py

--- employee calculator.py
+++ employee calculator.py
@@ -1,61 +1,68 @@
-# Import necessary libraries
 import streamlit as st
-
-# Define the Employee class
+ 
 class Employee:
-    def __init__(self, name, assigned_task, attendance, leaves, completed_tasks, communication_skills, team_collaboration, technical_skills):
+    def __init__(self, name, assigned_tasks, total_attendance, total_leaves):
         self.name = name
-        self.assigned_task = assigned_task
-        self.attendance = attendance
-        self.leaves = leaves
-        self.completed_tasks = completed_tasks
-        self.communication_skills = communication_skills
-        self.team_collaboration = team_collaboration
-        self.technical_skills = technical_skills
-
+        self.assigned_tasks = assigned_tasks
+        self.completed_tasks = 0
+        self.total_attendance = total_attendance
+        self.total_leaves = total_leaves
+ 
+    def complete_task(self, tasks_completed):
+        self.completed_tasks += tasks_completed
+ 
     def calculate_performance(self):
-        # Calculate performance based on attendance, completed tasks, and additional skills
-        performance = (
-            (self.attendance / 100) * 
-            (self.completed_tasks / self.assigned_task) * 
-            (self.communication_skills + self.team_collaboration + self.technical_skills) / 3
-        ) * 100
-        return performance
-
-# Function to display employee information and performance
-def display_employee_info(employee):
-    st.write(f"Name: {employee.name}")
-    st.write(f"Assigned Tasks: {employee.assigned_task}")
-    st.write(f"Attendance: {employee.attendance}%")
-    st.write(f"Leaves: {employee.leaves}")
-    st.write(f"Completed Tasks: {employee.completed_tasks}")
-    st.write(f"Communication Skills: {employee.communication_skills}")
-    st.write(f"Team Collaboration: {employee.team_collaboration}")
-    st.write(f"Technical Skills: {employee.technical_skills}")
-    st.write(f"Performance: {employee.calculate_performance():.2f}%")
-
-# Streamlit application
+        task_performance = (self.completed_tasks / self.assigned_tasks) * 100
+        attendance_percentage = (self.total_attendance / 30) * 100  # Assuming 30 working days in a month
+        # Penalty for leaves beyond 3
+        leaves_penalty = max(0, self.total_leaves - 3)
+        leave_percentage = max(0, 100 - (leaves_penalty * 5))  # Assuming a penalty of 5% for each leave beyond 3
+        overall_performance = (task_performance + attendance_percentage + leave_percentage) / 3
+        return overall_performance
+ 
+# Streamlit web application with a consolidated table for all employees
 def main():
-    # Title of the application
     st.title("Employee Performance Calculator")
-
-    # Get employee information from the user
-    name = st.text_input("Enter employee name:")
-    assigned_task = st.number_input("Enter the number of assigned tasks:", min_value=1)
-    attendance = st.number_input("Enter attendance percentage:", min_value=0, max_value=100)
-    leaves = st.number_input("Enter the number of leaves taken:")
-    completed_tasks = st.number_input("Enter the number of completed tasks:", min_value=0, max_value=assigned_task)
-    communication_skills = st.number_input("Enter communication skills rating (1-10):", min_value=1, max_value=10)
-    team_collaboration = st.number_input("Enter team collaboration rating (1-10):", min_value=1, max_value=10)
-    technical_skills = st.number_input("Enter technical skills rating (1-10):", min_value=1, max_value=10)
-
-    # Create an Employee object
-    employee = Employee(name, assigned_task, attendance, leaves, completed_tasks, communication_skills, team_collaboration, technical_skills)
-
-    # Display employee information and performance
-    if st.button("Calculate Performance"):
-        display_employee_info(employee)
-
-# Run the application
+ 
+    # Input for the number of employees
+    num_employees = st.number_input("Enter Number of Employees:", min_value=1, step=1)
+ 
+    # List to store Employee objects
+    employees = []
+ 
+    # Input for employee details
+    for i in range(num_employees):
+        st.header(f"Employee {i + 1}")
+        name = st.text_input(f"Enter Employee {i + 1} Name:")
+        assigned_tasks = st.number_input(f"Enter Number of Assigned Tasks for Employee {i + 1}:", min_value=1, step=1)
+        total_attendance = st.number_input(f"Enter Total Attendance for Employee {i + 1} (in days):", min_value=0, max_value=30, step=1)
+        total_leaves = st.number_input(f"Enter Total Leaves for Employee {i + 1} (in days):", min_value=0, max_value=30, step=1)
+ 
+        # Create an instance of the Employee class and add it to the list
+        employee = Employee(name, assigned_tasks, total_attendance, total_leaves)
+        employees.append(employee)
+ 
+        # Input for completed tasks
+        tasks_completed = st.number_input(f"Enter Number of Completed Tasks for Employee {i + 1}:", min_value=0, step=1)
+        employee.complete_task(tasks_completed)
+ 
+    # Display consolidated table for all employees
+    performance_data = []
+    for i, employee in enumerate(employees):
+        performance_percentage = employee.calculate_performance()
+ 
+        performance_data.append({
+            'Employee Name': employee.name,
+            'Assigned Tasks': employee.assigned_tasks,
+            'Completed Tasks': employee.completed_tasks,
+            'Attendance Percentage': (employee.total_attendance / 30) * 100,
+            'Leaves Taken': employee.total_leaves,
+            'Performance Percentage': performance_percentage
+        })
+ 
+    # Display consolidated table
+    st.subheader("Consolidated Employee Performance Table:")
+    st.table(performance_data)
+ 
 if __name__ == "__main__":
     main()
